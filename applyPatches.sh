5ac00c5fad72d4f9f9f3fcfa1cc5930ad8651b25
FakeNeth <cneth97@gmail.com>
2018-08-07 14:11:47 -0400

    fix build system issues

--- applyPatches.sh
+++ applyPatches.sh
@@ -6,6 +6,7 @@ echo "Rebuilding Forked projects.... "
 
 function applyPatch {
     what=$1
+	what_name=$(basename "$what")
     target=$2
     branch=$3
     cd "$basedir/$what"
@@ -17,20 +18,22 @@ function applyPatch {
         git clone "$what" "$target"
     fi
     cd "$basedir/$target"
-    echo "Resetting $target to $what..."
+	
+    echo "Resetting $target to $what_name..."
+	git remote rm upstream > /dev/null 2>&1
     git remote add -f upstream $basedir/$what >/dev/null 2>&1
-    git checkout master >/dev/null 2>&1
+    git checkout master 2>/dev/null || git checkout -b master
     git fetch upstream >/dev/null 2>&1
     git reset --hard upstream/upstream
 	
-	if [ ! -f "$basedir/${what}-Patches/"*.patch ]; then
+    if [ ! -f "$basedir/${what_name}-Patches/"*.patch ]; then
         echo "  No patches found to apply to $target!"
-		return 0
-	fi
+        return 0
+    fi
 
     echo "  Applying patches to $target..."
     git am --abort >/dev/null 2>&1
-    git am --3way --ignore-whitespace "$basedir/${what}-Patches/"*.patch
+    git am --3way --ignore-whitespace "$basedir/${what_name}-Patches/"*.patch
     if [ "$?" != "0" ]; then
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
@@ -47,6 +50,9 @@ basedir=$basedir/Paper
 
 applyPatch Bukkit Spigot-API HEAD && applyPatch CraftBukkit Spigot-Server patched
 applyPatch Spigot-API PaperSpigot-API HEAD && applyPatch Spigot-Server PaperSpigot-Server HEAD
-applyPatch PaperSpigot-API ../Scissors-API HEAD && applyPatch PaperSpigot-Server ../Scissors-Server HEAD
 
 popd
+
+basedir=$(dirname "$basedir")
+
+applyPatch Paper/PaperSpigot-API Scissors-API HEAD && applyPatch Paper/PaperSpigot-Server Scissors-Server HEAD
