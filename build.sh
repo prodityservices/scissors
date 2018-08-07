a861a7cd5dacfc4c5d74dfd732b731f2c102b221
FakeNeth <cneth97@gmail.com>
2018-08-07 08:52:39 -0400

    remove applyPatches.sh and replace with newApplyPatches.sh, rebrand pom.xml

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git submodule update --init --recursive && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
+git submodule update --init --recursive && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn clean install
 
 cp ./Scissors-Server/target/scissors*-SNAPSHOT.jar ./scissors.jar
