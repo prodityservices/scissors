a5c2bc6e7f8493729c583ca7f6308bd4b9d07422
FakeNeth <cneth97@gmail.com>
2018-08-09 07:53:05 -0400

    make build script more verbose

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+./clean.sh
+
 git submodule update --init --recursive && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn clean install
 
+echo " Deleting existing scissors.jar (if present)..."
+rm -rf ./scissors.jar
+
+echo " Copying new scissors.jar to root directory..."
 cp ./Scissors-Server/target/scissors*-SNAPSHOT.jar ./scissors.jar
+
+echo " "
+echo " Scissors build complete."
+echo " "
